8fa2fa6c597bfc9b656134f42093b119f8e565e2
Serhii8888 <137101635+Serhii8888@users.noreply.github.com>
2025-08-30 17:20:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -92,9 +92,19 @@ EOL
 step "Компіляція Trap"
 forge build
 
-# === drosera.toml ===
-step "Створення drosera.toml"
+# === Створення drosera.toml ===
+step "Формування drosera.toml"
+
+read -rp "Введіть вашу EVM адресу (для whitelist): " OPERATOR_ADDR
+
 cat > drosera.toml <<EOL
+ethereum_rpc = "https://ethereum-hoodi-rpc.publicnode.com"
+drosera_rpc = "https://relay.hoodi.drosera.io"
+eth_chain_id = 560048
+drosera_address = "0x91cB447BaFc6e0EA0F4Fe056F5a9b1F14bb06e5D"
+
+[traps]
+
 [traps.mytrap]
 path = "out/HelloWorldTrap.sol/HelloWorldTrap.json"
 response_contract = "0x183D78491555cb69B68d2354F7373cc2632508C7"
@@ -106,6 +116,7 @@ block_sample_size = 10
 private_trap = true
 whitelist = ["$OPERATOR_ADDR"]
 EOL
+
 
 # === Deploy Trap ===
 step "Деплой Trap"
